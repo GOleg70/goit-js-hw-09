c305c79d42c9837e71a5845384bad3ba732414c0
Oleg Ganus <oleg110770@gmail.com>
2025-05-28 16:42:46 +0300

    end home work 9

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,45 @@
-console.log("Form");
+const form = document.querySelector(".feedback-form");
+const STORAGE_KEY = "feedback-form-state";
+
+let formData = {
+  email: "",
+  message: ""
+};
+
+const saved = localStorage.getItem(STORAGE_KEY);
+if (saved) {
+    
+  try {
+    const parsed = JSON.parse(saved);
+    formData = { ...formData, ...parsed };
+    form.elements.email.value = parsed.email || "";
+    form.elements.message.value = parsed.message || "";
+       
+  } catch (error) {
+    console.error("Помилка при читанні з localStorage:", error);
+  }
+}
+
+form.addEventListener("input", event => {
+
+  const { name, value } = event.target;
+  formData[name] = value.trim(); // оновлюємо тільки одне поле
+  
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+
+form.addEventListener("submit", event => {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert("Fill please all fields");
+    return;
+  }
+
+  console.log(formData);
+
+  form.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  formData = { email: "", message: "" };
+});
